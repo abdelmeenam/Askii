f439c02131afe32860b1e834b0126b8f1b537af2
abdelmeenam <abdelmeenam@gmail.com>
2023-08-29 23:58:10 +0300

    Roles&Permissions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,6 +11,12 @@ use Illuminate\Support\Facades\Gate;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(['auth' ,'checkUserType:admin,super-admin']);
+    }
+
     // show all users with their roles
     public function index()
     {
