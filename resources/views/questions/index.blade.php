21e85885804286ddfe192d6a1779119648ea371b
abdelmeenam <abdelmeenam@gmail.com>
2024-10-19 22:02:43 +0300

    Refactor code

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -63,14 +63,14 @@
                             <div>
                                 {{--                        <a href="{{route('questions.show' , $question->id) }}" class="btn btn-outline-primary btn-sm">View</a> --}}
                                 <a href="{{ route('questions.edit', $question->id) }}"
-                                    class="btn btn-outline-info btn-sm">Edit</a>
+                                    class="btn btn-outline-info btn-sm">{{ __('Edit') }}</a>
                             </div>
                             <div>
                                 <form id="deleteQuestionForm" action="{{ route('questions.destroy', $question->id) }}"
                                     method="post" class="delete-form">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
+                                    <button type="submit" class="btn btn-outline-danger btn-sm">{{ __('Delete') }}</button>
                                 </form>
                             </div>
                         </div>
@@ -95,12 +95,12 @@
     </div>
     <script>
         /**
-                                                                                                                                        setTimeout(function() {
-                                                                                                                                            // document.querySelector('.alert').remove();
-                                                                                                                                            //document.querySelector('.alert').style.display = 'none';
-                                                                                                                                            document.querySelector('.alert') ? document.querySelector('.alert').style.display = 'none' : '';
-                                                                                                                                        }, 4000);
-                                                                                                                                **/
+                                                                                                                                                setTimeout(function() {
+                                                                                                                                                    // document.querySelector('.alert').remove();
+                                                                                                                                                    //document.querySelector('.alert').style.display = 'none';
+                                                                                                                                                    document.querySelector('.alert') ? document.querySelector('.alert').style.display = 'none' : '';
+                                                                                                                                                }, 4000);
+                                                                                                                                        **/
 
         document.addEventListener('DOMContentLoaded', function() {
             let deleteForms = document.querySelectorAll('.delete-form');
